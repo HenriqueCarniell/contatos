4882f819455aff5518fc84775bc9cda6cdcd8568
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-02-11 12:06:04 -0300

    first commit

--- client/controllers/sendadoscontroller.js
+++ client/controllers/sendadoscontroller.js
@@ -1,35 +1,55 @@
 const db = require('../db/db');
+const bcrypt = require('bcrypt')
 
 exports.Send = async (req, res) => {
     const { nome, email, senha } = req.body;
     console.log(nome,email,senha);
 
-    ValidaDadosEmBranco(nome, email, senha, res);
+    ValidaDadosEmBranco(nome, email, senha);
     let dadoBd = await ValidaDadosBancoDeDados(email,res);
 
     if(dadoBd === true) {
         res.status(501).json({msg: "Esse email já foi usado por favor use outro"});
     } else {
+        await inserirBancodeDados(nome,email,senha)
         res.status(201).json({msg: "Conta Cadastrada com sucesso"});
     }
 }
 
 // Funções
+let inserirBancodeDados = async (nome,email,senha,res) => {
+        const sql = "insert into usuario(Nome,Email,Senha) values (?,?,?)";
+
+        const salt = await bcrypt.genSalt(12);
+        const senhaCript = await bcrypt.hash(senha,salt);
+
+        db.query(sql,[nome,email,senhaCript], (err,result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(result);
+            }
+        })
+}
 
 // Validação
 let ValidaDadosEmBranco = (nome, email, senha, res) => {
     if (!nome) {
         res.status(400).json({msg: "O nome deve ser preenchido"});
+        return false;
     }
     if (!email) {
         res.status(400).json({msg: "O email deve ser preenchido"});
+        return false;
     }
     if (!senha) {
         res.status(400).json({msg: "A senha deve ser preenchida"});
+        return false;
     }
+    return true;
 }
 
-let ValidaDadosBancoDeDados = (email,res) => {
+let ValidaDadosBancoDeDados = (email) => {
     return new Promise((resolve,reject) => {
         const sql = "select email from usuario where email = ?";
 
